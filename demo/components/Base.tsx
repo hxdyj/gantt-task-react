80eb131d565ceaf7f758f1c513a3737c1fed1ed8
songle <951540966@qq.com>
2024-08-14 17:26:13 +0800

    feat: lodash

--- demo/components/Base.tsx
+++ demo/components/Base.tsx
@@ -1,18 +1,19 @@
+import { range } from 'lodash';
 import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from '../../src/index';
 
 export function Base() {
-  let tasks: Task[] = [
-    {
+  let tasks: Task[] = range(10).map(item => {
+    return {
       start: new Date(2020, 1, 1),
       end: new Date(2020, 1, 2),
-      name: 'Idea',
-      id: 'Task 0',
-      type: 'task',
+      name: 'Idea' + item,
+      id: 'Task' + item,
+      type: 'task' + item,
       progress: 100,
       isDisabled: true,
       styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-    },
-  ]
+    }
+  })
   return (
     <div className="page-Base">
       <Gantt tasks={tasks} startDate={new Date(2020, 1, 1)} viewMode={
